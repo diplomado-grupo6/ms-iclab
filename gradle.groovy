297532e1a04867392f74c4d59998a0d65fb93226
mauroch20 <56987870+mauroch20@users.noreply.github.com>
2022-02-04 21:32:39 -0300

    Update gradle.groovy

---
+++ gradle.groovy
@@ -0,0 +1,157 @@
+def call(){
+
+        if(isIcOrRelease()=='CI'){
+        figlet 'CI'
+        stage('compile-unitTest-jar') {
+            STAGE=env.STAGE_NAME
+            figlet 'compile'
+            figlet 'unitTest'
+            figlet 'jar'
+
+            sh 'chmod a+x gradlew'
+            sh './gradlew build'
+        }
+        
+        stage('sonar') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            scannerHome = tool 'sonar-scanner'
+            withSonarQubeEnv('sonarqube-server') {
+                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven -Dsonar.java.binaries=build"
+            }
+        }
+        
+        stage('nexusUpload') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            nexusPublisher nexusInstanceId: 'nexus_test', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'ejemplo-maven-feature-sonar', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+        }
+        if(getBranchType()=='develop'){
+            stage('gitCreateRelease') {
+                //solo rama dev
+                STAGE=env.STAGE_NAME
+                figlet STAGE
+                String rama="release-v1-0-2"
+                createBranch('develop',rama)
+
+            }
+        }
+    }
+    else{
+
+        //CD
+        figlet 'CD'
+        stage('gitDiff') {
+            
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+        }
+        
+        stage('nexusDownload') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            sh 'curl -X GET -u admin:L1m1t2rm., http://localhost:8082/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
+        }
+        
+        stage('run') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            sh 'nohup bash gradlew bootRun &'
+            sleep(20)
+        }
+        stage('test') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            sh """curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"""
+        }
+        stage('gitMergeMaster') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            
+        }
+        stage('gitMergeDevelop') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            
+        }
+        stage('gitTagMaster') {
+            STAGE=env.STAGE_NAME
+            figlet STAGE
+            
+        }
+    }
+     
+}
+
+def getBranchType(){
+    String gitBranch=env.GIT_BRANCH
+    if(gitBranch.contains('feature-') ){
+        return 'feature'
+    }
+    else if(gitBranch.contains('develop') ){
+        return 'develop'
+    }
+    else if(gitBranch.contains('release-') ){
+        return 'release'
+    }
+    error "Undefined branch name"
+
+}
+def isIcOrRelease(){
+    String branchType=getBranchType()
+    if( branchType=='feature' ||  
+        branchType=='develop'){
+            return 'CI'
+    }
+    else if(branchType=='release'){
+        return 'Release'
+    }
+        
+    error "Undefined branch type ${branchType}"
+    
+
+}
+def createBranch(String ramaOrigen,String ramaNueva){
+    checkout(ramaOrigen)
+    sh """
+		git checkout -b ${ramaNueva}
+		git push origin ${ramaNueva}
+		
+	"""
+
+
+}
+
+def merge(String ramaOrigen, String ramaDestino){
+	println "Este método realiza un merge ${ramaOrigen} y ${ramaDestino}"
+
+	checkout(ramaOrigen)
+	checkout(ramaDestino)
+
+	sh """
+		git merge ${ramaOrigen}
+		git push origin ${ramaDestino}
+	"""
+}
+
+def tag(String ramaOrigen, String ramaDestino){
+	println "Este método realiza un tag en master de ${ramaOrigen}"
+
+	if (ramaOrigen.contains('release-v')){
+		checkout(ramaDestino)
+		def tagValue = ramaOrigen.split('release-')[1]
+		sh """
+			git tag ${tagValue}
+			git push origin ${tagValue}
+		"""
+
+	} else {
+		error "La rama ${ramaOrigen} no cumple con nomenclatura definida para rama release (release-v(major)-(minor)-(patch))."
+	}
+}
+
+def checkout(String rama){
+	sh "git reset --hard HEAD; git checkout ${rama}; git pull origin ${rama}"
+}
+
+return this;
